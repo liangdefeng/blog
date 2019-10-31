277271f6d91646070b28f3d077f4f64d1aa05973
Peter Liang <defeng.liang@yahoo.com>
2019-10-31 17:50:54 +0800

    Add additional features.

---
+++ modules/web/src/com/spel/blog/web/screens/ArticleContentEditScreen.java
@@ -0,0 +1,10 @@
+package com.spel.blog.web.screens;
+
+import com.haulmont.cuba.gui.screen.Screen;
+import com.haulmont.cuba.gui.screen.UiController;
+import com.haulmont.cuba.gui.screen.UiDescriptor;
+
+@UiController("blog_ArticleContentEditScreen")
+@UiDescriptor("article-content-edit-screen.xml")
+public class ArticleContentEditScreen extends Screen {
+}
--- modules/web/src/com/spel/blog/web/screens/article/ArticleEdit.java
+++ modules/web/src/com/spel/blog/web/screens/article/ArticleEdit.java
@@ -1,5 +1,11 @@
 package com.spel.blog.web.screens.article;
 
+import com.haulmont.cuba.core.global.EntityStates;
+import com.haulmont.cuba.core.global.Messages;
+import com.haulmont.cuba.gui.components.*;
+import com.haulmont.cuba.gui.components.actions.BaseAction;
+import com.haulmont.cuba.gui.icons.CubaIcon;
+import com.haulmont.cuba.gui.icons.Icons;
 import com.haulmont.cuba.gui.model.DataContext;
 import com.haulmont.cuba.gui.model.InstanceContainer;
 import com.haulmont.cuba.gui.screen.*;
@@ -13,14 +19,48 @@ import javax.inject.Inject;
 @EditedEntityContainer("articleDc")
 @LoadDataBeforeShow
 public class ArticleEdit extends StandardEditor<Article> {
+
     @Inject
     private InstanceContainer<Article> articleDc;
 
     @Inject
     private UserSession userSession;
 
+    String POOPUP_MESSAGE = "popupMessage";
+
+    protected void initActions(@SuppressWarnings("unused") InitEvent event) {
+
+        super.initActions(event);
+
+        Window window = getWindow();
+        Messages messages = getBeanLocator().get(Messages.NAME);
+        Icons icons = getBeanLocator().get(Icons.NAME);
+
+        Action popupMessageAction = new BaseAction(POOPUP_MESSAGE)
+                .withIcon(icons.get(CubaIcon.EDITOR_CANCEL))
+                .withCaption(messages.getMainMessage("actions.Cancel"))
+                .withHandler(this::popupMessage);
+
+        window.addAction(popupMessageAction);
+    }
+
+    protected void popupMessage(@SuppressWarnings("unused") Action.ActionPerformedEvent event) {
+        this.getWindow().showMessageDialog("Debug", event.getSource().getCaption(), Frame.MessageType.WARNING);
+    }
+
+    private EntityStates getEntityStates() {
+        return getBeanLocator().get(EntityStates.NAME);
+    }
+
+    @Subscribe("contentField")
+    public void onContentFieldValueChange(HasValue.ValueChangeEvent<String> event) {
+
+    }
+
     @Subscribe(target = Target.DATA_CONTEXT)
     public void onPreCommit(DataContext.PreCommitEvent event) {
-        articleDc.getItem().setAuthor(userSession.getUser());
+        if (getEntityStates().isNew(getEditedEntity()) || doNotReloadEditedEntity()) {
+            articleDc.getItem().setAuthor(userSession.getUser());
+        }
     }
 }
