3a10822b23b557de7799b268a72981dd9204ed2c
Peter Liang <defeng.liang@yahoo.com>
2019-11-08 09:11:28 +0800

    Add login and main screen.

---
+++ modules/web/src/com/spel/blog/web/screens/ExtLoginScreen.java
@@ -0,0 +1,11 @@
+package com.spel.blog.web.screens;
+
+import com.haulmont.cuba.gui.screen.UiController;
+import com.haulmont.cuba.gui.screen.UiDescriptor;
+import com.haulmont.cuba.web.app.login.LoginScreen;
+
+
+@UiController("login")
+@UiDescriptor("ext-login-screen.xml")
+public class ExtLoginScreen extends LoginScreen {
+}
